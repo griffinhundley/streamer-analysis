686b9c479585862e7f96249b1b284fe0ffe868d3
Griffin Hundley <hundlegq@dukes.jmu.edu>
2021-01-07 13:52:25 -0500

    updated images

--- src/eda.py
+++ src/eda.py
@@ -6,14 +6,15 @@ def hist_plot(column):
     df = pd.read_csv('../data/streamer_data.csv')
     df = df.drop(columns = ['game_name', 'login', 'broadcaster_type', 'language'])
     df.account_age = pd.to_timedelta(df.account_age).map(lambda x: x.days)
-    
+    column_title = column.replace('_', ' ').title()
     fig, ax = plt.subplots(figsize = (16,8))
-    ax.set_title(f'Unpartnered vs Partnered Distribution of {column}', fontsize = 24)
+    ax.set_title(f'Unpartnered vs Partnered Distribution of {column_title}', fontsize = 32)
     sns.distplot(df.loc[df.target == 0][column], ax = ax)
     sns.distplot(df.loc[df.target == 1][column], ax = ax)
-    ax.tick_params(labelsize=14)
-    ax.set_xlabel(ax.get_xlabel(), fontsize = 16)
-    ax.legend(['Unpartnered', 'Partnered'], fontsize = 16)
+    ax.tick_params(labelsize=18)
+    ax.set_xlabel(column_title, fontsize = 24)
+    ax.set_ylabel('Frequency', fontsize = 24)
+    ax.legend(['Unpartnered', 'Partnered'], fontsize = 24)
     return fig
 
 
@@ -22,11 +23,13 @@ def scatter_fig(var1, var2):
     df = df.drop(columns = ['game_name', 'login', 'broadcaster_type', 'language'])
     df.account_age = pd.to_timedelta(df.account_age).map(lambda x: x.days)
     fig, ax = plt.subplots(figsize = (16,8))
-    ax.set_title(f'{var1} vs {var2}', fontsize = 24)
-    ax.set_xlabel(ax.get_xlabel(), fontsize = 16)
-    ax.set_ylabel(ax.get_ylabel(), fontsize = 16)
-    ax.tick_params(labelsize = 14)
+    var1_title = var1.replace('_', ' ').title()
+    var2_title = var2.replace('_', ' ').title()
+    ax.set_title(f'{var1_title} vs {var2_title}', fontsize = 32)
+    ax.tick_params(labelsize = 18)
     sns.scatterplot(df.loc[df.target == 0][var1], df.loc[df.target == 0][var2], alpha=0.5);
     sns.scatterplot(df.loc[df.target == 1][var1], df.loc[df.target == 1][var2], alpha=0.5);
-    ax.legend(['Unpartnered', 'Partnered'], fontsize = 16)
+    ax.set_xlabel(var1_title, fontsize = 24)
+    ax.set_ylabel(var2_title, fontsize = 24)
+    ax.legend(['Unpartnered', 'Partnered'], fontsize = 24)
     return fig
